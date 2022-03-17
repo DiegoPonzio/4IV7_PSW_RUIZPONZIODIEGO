bacd5e339bbd19ca768dd347f8fa2d2604aded3a
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-03-16 22:17:06 -0600

    Algoritmia

--- 07AlgoritmiaJs/js/logica.js
+++ 07AlgoritmiaJs/js/logica.js
@@ -1,3 +1,50 @@
+//problema 2
+
+function problema2(){
+    if(document.getElementById("p2-x1").value == ""){
+        document.getElementById("p2-x1").value = 0;
+    }
+    if(document.getElementById("p2-x2").value == ""){
+        document.getElementById("p2-x2").value = 0;
+    }
+    if(document.getElementById("p2-x3").value == ""){
+        document.getElementById("p2-x3").value = 0;
+    }
+    if(document.getElementById("p2-x4").value == ""){
+        document.getElementById("p2-x4").value = 0;
+    }
+    if(document.getElementById("p2-x5").value == ""){
+        document.getElementById("p2-x5").value = 0;
+    }
+    if(document.getElementById("p2-y1").value == ""){
+        document.getElementById("p2-y1").value = 0;
+    }
+    if(document.getElementById("p2-y2").value == ""){
+        document.getElementById("p2-y2").value = 0;
+    }
+    if(document.getElementById("p2-y3").value == ""){
+        document.getElementById("p2-y3").value = 0;
+    }
+    if(document.getElementById("p2-y4").value == ""){
+        document.getElementById("p2-y4").value = 0;
+    }
+    if(document.getElementById("p2-y5").value == ""){
+        document.getElementById("p2-y5").value = 0;
+    }
+    var x1 = parseInt(document.getElementById("p2-x1").value);
+    var x2 = parseInt(document.getElementById("p2-x2").value);
+    var x3 = parseInt(document.getElementById("p2-x3").value);
+    var x4 = parseInt(document.getElementById("p2-x4").value);
+    var x5 = parseInt(document.getElementById("p2-x5").value);
+    var y1 = parseInt(document.getElementById("p2-y1").value);
+    var y2 = parseInt(document.getElementById("p2-y2").value);
+    var y3 = parseInt(document.getElementById("p2-y3").value);
+    var y4 = parseInt(document.getElementById("p2-y4").value);
+    var y5 = parseInt(document.getElementById("p2-y5").value);
+    var vector = x1*y1 + x2*y2 + x3*y3 + x4*y4 + x5*y5;
+    document.getElementById("p2-output").innerHTML = "EL valor del vector es: " + vector
+}
+
 //problema 3
 
 function problema3(){
