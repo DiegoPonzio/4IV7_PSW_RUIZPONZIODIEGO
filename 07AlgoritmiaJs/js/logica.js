a90417f57d0a8ec420a2c3cc2877fbcb403ef006
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-03-20 16:16:19 -0600

    Algoritmia JS

--- 07AlgoritmiaJs/js/logica.js
+++ 07AlgoritmiaJs/js/logica.js
@@ -1,4 +1,11 @@
 //problema 1
+function validarn(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8 || teclado == 32) return true;
+    var patron = /[q w e r t y u i o p a s d f g h j k l ñ z x c v b n m Q W E R T Y U I O P A S D F G H J K L Ñ Z X C V B N M]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
 function reverse(S){
     return S.split("").reverse().join("")
 }
